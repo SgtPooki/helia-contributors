e4a45b1a1068b4526f16fd8ab7bf947033605d0f
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-09-07 14:04:33 -0700

    fix: update package name and github packages

--- config.js
+++ config.js
@@ -1,11 +1,35 @@
 export default {
   repos: [
+    // helia-named repos in ipfs org
     'ipfs/helia',
+    'ipfs/helia-car',
+    'ipfs/helia-cli',
+    'ipfs/helia-dag-cbor',
+    'ipfs/helia-dag-json',
+    'ipfs/helia-ipns',
+    'ipfs/helia-json',
+    'ipfs/helia-mfs',
+    'ipfs/helia-remote-pinning',
+    'ipfs/helia-routing-v1-http-api',
+    'ipfs/helia-strings',
+    'ipfs/helia-unixfs',
+    'ipfs/js-stores',
 
+    // non helia-specific repos in ipfs org
+    'ipfs/js-ipfs-bitswap',
+    'ipfs/js-ipns',
+
+    // no need to check individual repos since this repo contains the source of truth for all helia-examples
     'ipfs-examples/helia-examples',
 
-    'libp2p/js-libp2p',
+    // ipfs-shipyard repos
+    'ipfs-shipyard/helia-contributors',
+    'ipfs-shipyard/helia-css',
+    'ipfs-shipyard/helia-service-worker-gateway',
+    'ipfs-shipyard/www-helia-identify',
 
+    // partner deps
+    'ChainSafe/gossipsub-js'
   ],
   bots: [
     'dependabot',
